5a92488d494a0886891787ed01a3b063caa0bd43
Warif Akhand Rishi <rishi420@gmail.com>
2017-07-19 17:23:26 +0600

    Available numbers, incoming numbers, send message and buy a number api added

--- TwilioTest/ViewController.swift
+++ TwilioTest/ViewController.swift
@@ -10,38 +10,76 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    let url = "https://api.twilio.com/2010-04-01/Accounts/\(K.Twilio.SID)/Messages.json"
-    
-    // credentials
-    let credentialData = "\(K.Twilio.SID):\(K.Twilio.authToken)".utf8
-    let base64Credentials = Data(credentialData).base64EncodedString()
-    let headers = ["Authorization": "Basic \(base64Credentials)"]
-    
-    // params
-    let params: [String: String] = ["From": "FROM_NUMBER",
-                                    "To": "TO_NUMBER",
-                                    "Body": "Hello from Twilio"]
-    
-    Alamofire.request(url, method: .post, parameters: params,
-                      encoding: URLEncoding.default, headers: headers)
-      .authenticate(user: K.Twilio.SID,
-                    password: K.Twilio.authToken).responseJSON { response in
-                      
-                      switch response.result {
-                      case .success:
-                        print("success \(response)")
-                      case .failure(let error):
-                        print(error)
-                      }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
     
-  }
+    func postRequest(url: String, method: HTTPMethod, params: [String: Any]? = .none) {
+        // credentials
+        let credentialData = "\(K.Twilio.SID):\(K.Twilio.authToken)".utf8
+        let base64Credentials = Data(credentialData).base64EncodedString()
+        let headers = ["Authorization": "Basic \(base64Credentials)"]
+        
+        Alamofire.request(url, method: method, parameters: params,
+                          encoding: URLEncoding.default, headers: headers)
+            .authenticate(user: K.Twilio.SID,
+                          password: K.Twilio.authToken).responseJSON { response in
+                            
+                            switch response.result {
+                            case .success:
+                                print("success \(response)")
+                            case .failure(let error):
+                                print(error)
+                            }
+        }
+    }
+    
+    @IBAction func availableNumbersButtonAciton(_ sender: Any) {
+        print("availableNumbersButtonAciton")
+        
+        let url = "https://api.twilio.com/2010-04-01/Accounts/\(K.Twilio.SID)/AvailablePhoneNumbers/US/Local.json"
+        
+        // params
+        let params = ["AreaCode": "510", "SmsEnabled": "True"]
+        //let params = ["InRegion": "AR"]
+        
+        postRequest(url: url, method: .get, params: params)
+    }
   
+    @IBAction func IncomingNumbersButtonAciton(_ sender: Any) {
+        print("IncomingNumbersButtonAciton")
+        
+        let url = "https://api.twilio.com/2010-04-01/Accounts/\(K.Twilio.SID)/IncomingPhoneNumbers.json"
+        
+        postRequest(url: url, method: .get)
+    }
+    
+    @IBAction func sendMessageButtonAciton(_ sender: Any) {
+        print("sendMessageButtonAciton")
+        
+        let url = "https://api.twilio.com/2010-04-01/Accounts/\(K.Twilio.SID)/Messages.json"
+        
+        // params
+        let params: [String: String] = ["From": "FROM_NUMBER",
+                                        "To": "TO_NUMBER",
+                                        "Body": "Hello from Twilio, Rishi123"]
+        
+        postRequest(url: url, method: .post, params: params)
+    }
+    
+    @IBAction func buyNumberButtonAciton(_ sender: Any) {
+        print("buyNumberButtonAciton")
+        
+        let url = "https://api.twilio.com/2010-04-01/Accounts/\(K.Twilio.SID)/IncomingPhoneNumbers.json"
+        
+        let params = ["AreaCode": "510"]
+        //let params = ["PhoneNumber": "+15103691691"]
+        
+        postRequest(url: url, method: .post, params: params)
+    }
+    
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
